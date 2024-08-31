f947d3a300fc3ca3963ea679d6544695aaa5e515
brqd <hubert@kolodziejski.eu>
2024-08-31 10:33:17 +0200

    aging decremented

--- state/__init__.py
+++ state/__init__.py
@@ -10,7 +10,7 @@ picture_conf = ""
 picture_path = ""
 
 points_count = 60
-clean_points_period = 1 # seconds until points are considered as invalid (too old)
+clean_points_period = 0.5 # seconds until points are considered as invalid (too old)
 validation_period = 1 # seconde until area is considered as invalid and have to be recalculated
 area_sensitivity = 10 # minimal number of valid points to activate area
 
